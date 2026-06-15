021655c4c26dfed9ce40be50f029a52eb9d659f4
ChriisCG3312 <gchristiancueto024@gmail.com>
2026-06-15 08:59:45 -0600

    Revert "Feature/visuals navflow"

--- config/wsgi.py
+++
@@ -1,6 +0,0 @@
-"""WSGI config para el proyecto Detailing."""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-application = get_wsgi_application()
